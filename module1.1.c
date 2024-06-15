7f26429a347f5c6d35161cb6683702fee323f6a5
ansitops07 <aansigopani493@gmail.com>
2024-06-15 16:47:02 +0530

    Initial commit

---
+++ module1.1.c
@@ -0,0 +1,13 @@
+//Display This Information using printf
+//a. Your Name
+//b. Your Birth date
+//c. Your Age
+//d. Your Address
+
+#include<stdio.h>
+void main(){
+    printf("my name is:Aansi\n");
+    printf("my birth date is:21 march,2007\n");
+    printf("my age is:17\n");
+    printf("my address is:A-1104,apple harmony,near maharaja farm,mota varachha,surat\n");
+}
